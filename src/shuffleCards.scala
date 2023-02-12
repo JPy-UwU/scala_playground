95e1b10b6d33f6ea15b310b4738aa5732451dde9
Jay <jay.patel.py@gmail.com>
2023-02-11 22:33:28 -0600

    Initial commit - added shuffleCards to project

---
+++ src/shuffleCards.scala
@@ -0,0 +1,15 @@
+/**
+ * This file contains functions to perform a Faro Suffle.
+ * A perfect Faro shuffle breaks up the stack into two sub-stacks of exactly the same size,
+ * and then combines them in the manner described above.
+ * An out-shuffle results in the top and the bottom cards of the stack remaining the same after the shuffle;
+ * an in-shuffle results in these cards becoming the second and the second last cards of the shuffled stack.
+ */
+object Main {
+
+  
+  def main(args: Array[String]): Unit = {
+    //TODO: write test cases each function
+
+  }
+}
